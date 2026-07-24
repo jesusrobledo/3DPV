3a01caa5df2dacb0825116337a9e3b6cca9c7954
jesus <jesus.robledo@lucisun.com>
2026-07-24 18:03:32 +0200

    v1.0 - rc1 Test for multiple FBOs

--- static/js/Core.js
+++ static/js/Core.js
@@ -9,6 +9,7 @@ export class CoreObject{
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.shaders = null;
+        this.gl = this.renderer.getContext();
         document.body.appendChild( this.renderer.domElement );
     }
     init(){
@@ -40,9 +41,24 @@ export class CoreObject{
     async createGPUEnvironment(){
         let shaderData = [];
         shaderData.push({name:'Test',file:'Test.shd'});
-        shaderData.push({name:'Test2',file:'Test2.shd'});
 
         this.shaders = await Promise.all(shaderData.map(data => this.loadShaderData(data)));
         let x = 0;
+    }
+}
+export class FBO{
+    constructor(gl,width,height){
+        this.framebuffer = gl.createFramebuffer();
+        this.texture = gl.createTexture();
+        gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA8,width,height,0,gl.RGBA,gl.UNSIGNED_BYTE,null);
+        gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER,gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER,gl.COLOR_ATTACHMENT0,gl.TEXTURE_2D,this.texture,0);
+		gl.bindTexture(gl.TEXTURE_2D,null);
+		gl.bindFramebuffer(gl.FRAMEBUFFER,null);
     }
 }
--- static/js/GUI.js
+++ static/js/GUI.js
@@ -1,6 +1,16 @@
 import {CoreObject} from "./Core.js"
+import {FBO} from "./Core.js"
 
 const coreObject = new CoreObject();
 await coreObject.createGPUEnvironment();
 coreObject.init();
 coreObject.renderer.setAnimationLoop( coreObject.animate );
+
+const test = {};
+
+let N = 500000;
+for (let n=0;n<N;n++){
+    test[n] = new FBO(coreObject.gl,4096,4096);
+    if (n==N-1)
+        alert ("OK");
+}
