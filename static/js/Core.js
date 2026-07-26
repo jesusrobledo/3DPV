71b3779f8411ac4cde83ef25d9840d80fb87427f
jesus <jesus.robledo@lucisun.com>
2026-07-26 18:45:46 +0200

    v1.0 - Update 26/07/2026 - 18:45

--- static/js/Core.js
+++ static/js/Core.js
@@ -1,5 +1,182 @@
 import * as THREE from 'three';
+export class FBO{
+    constructor(gl,width,height){
+        this.framebuffer = gl.createFramebuffer();
+        this.texture = gl.createTexture();
+        this.width = width;
+        this.height = height;
+        gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA,width,height,0,gl.RGBA,gl.UNSIGNED_BYTE,null);
+        gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER,gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER,gl.COLOR_ATTACHMENT0,gl.TEXTURE_2D,this.texture,0);
+		gl.bindTexture(gl.TEXTURE_2D,null);
+		gl.bindFramebuffer(gl.FRAMEBUFFER,null);
+    }
+}
+export class Shader{
+    constructor(gl,data){
 
+        // This method includes the basic definition of the shader obtained from
+        // external files and create the backbone in terms of the vertex and
+        // fragment shaders and their associated attributes and uniforms
+
+        this.webGLObject = {};
+        let errorLog = "";
+
+        // Include vertex shader
+        this.vertexShader = gl.createShader(gl.VERTEX_SHADER);
+		gl.shaderSource(this.vertexShader,data.vertexShader);
+		gl.compileShader(this.vertexShader);
+		if ( gl.getShaderInfoLog(this.vertexShader) !== '' )
+			errorLog += "VertexShader: \n" + gl.getShaderInfoLog(this.vertexShader)+"\n";
+
+        // Include fragment shader
+        this.fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+		gl.shaderSource(this.fragmentShader,data.fragmentShader);
+		gl.compileShader(this.fragmentShader);
+		if ( gl.getShaderInfoLog(this.fragmentShader) !== '' )
+			errorLog += "FragmentShader: \n" + gl.getShaderInfoLog(this.fragmentShader)+"\n";
+
+		if (errorLog != "")
+		    alert (errorLog);
+
+        // Link the program
+        this.program = gl.createProgram();
+		gl.attachShader(this.program,this.vertexShader);
+		gl.attachShader(this.program,this.fragmentShader);
+		gl.linkProgram(this.program);
+
+		// Allocate attributes
+		this.attributes = [];
+		for (let i=0;i<data.attributes.length;i++){
+			this.program[data.attributes[i].name] = gl.getAttribLocation(this.program,data.attributes[i].name);
+			this.attributes.push(data.attributes[i]);
+		}
+
+		// Allocate uniforms
+		this.uniforms = [];
+		for (let i=0;i<data.uniforms.length;i++){
+			this.program[data.uniforms[i].name] = gl.getUniformLocation(this.program,data.uniforms[i].name);
+			this.uniforms.push(data.uniforms[i]);
+		}
+    }
+	createWebGL2BufferArrays(gl,items){
+
+        const dataType = {'float':gl.FLOAT};
+        for (const item of Object.values(items)){
+		    this.webGLObject[item.name] = {};
+		    this.webGLObject[item.name]['vao'] = gl.createVertexArray();
+		    this.webGLObject[item.name]['nVertices'] = item.nVertices;
+
+		    gl.bindVertexArray(this.webGLObject[item.name].vao);
+		    for (const attribute of Object.values(this.attributes)){
+                gl.bindBuffer(gl.ARRAY_BUFFER,gl.createBuffer());
+                gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(item.vertexData[attribute.name]),gl.STATIC_DRAW);
+                let attrLoc = gl.getAttribLocation(this.program,attribute.name);
+                gl.enableVertexAttribArray(attrLoc);
+                gl.vertexAttribPointer(attrLoc,
+                                       attribute.size,
+                                       dataType[attribute.type],
+                                       false,
+                                       attribute.size*Float32Array.BYTES_PER_ELEMENT,
+                                       0);
+            }
+            gl.bindVertexArray(null);
+        }
+	}
+	executeGPUCode(gl,FBO,uniforms){
+
+	    gl.useProgram(this.program);
+
+	    gl.bindFramebuffer(gl.FRAMEBUFFER,FBO.frameBuffer);
+		gl.bindTexture(gl.TEXTURE_2D,FBO.texture);
+
+		gl.viewport(0,0,FBO.width,FBO.height);
+	    gl.clearColor(0.5,0.5,0.5,1.0);
+	    gl.clear(gl.COLOR_BUFFER_BIT);
+
+        for (let id in uniforms){
+            gl.uniform3f(gl.getUniformLocation(this.program, id), uniforms[id][0],uniforms[id][1],uniforms[id][2]);
+        }
+
+        for (let item of Object.values(this.webGLObject)){
+            gl.bindVertexArray(item.vao);
+	        gl.drawArrays(gl.POINTS,0,item.nVertices);
+	    }
+
+	    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+		gl.bindTexture(gl.TEXTURE_2D, null);
+	}
+}
+export class GPUEngine{
+    constructor(){
+        this.gl = canvas.getContext("webgl2");
+        this.shaders = {};
+        this.FBO = {};
+    }
+    async loadShader (data){
+        const response = await fetch('/load_shader_code', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({shaderFile:data.file})
+        });
+        const responseData = await response.json();
+        return {
+            name:data.name,
+            vertexShader:responseData.vertexShader,
+            fragmentShader:responseData.fragmentShader,
+            attributes:Object.values(responseData.attributes),
+            uniforms:Object.values(responseData.uniforms)
+        };
+    }
+    async createGPUEnvironment(){
+        let shadersDefinition = [];
+        let shadersData = [];
+        shadersDefinition.push({name:'test',file:'Test.shd'});
+        shadersData = await Promise.all(shadersDefinition.map(data => this.loadShader(data)));
+        for (const shaderData of shadersData){
+            this.shaders[shaderData.name] = new Shader(this.gl,shaderData);
+        }
+    }
+    downloadTexture(name,fileName){
+        const pixels = new Uint8Array(this.FBO[name].width * this.FBO[name].height * 4);
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.FBO[name].frameBuffer);
+        this.gl.readPixels(0, 0, this.FBO[name].width, this.FBO[name].height, this.gl.RGBA, this.gl.UNSIGNED_BYTE, pixels);
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+
+        const canvas2d = document.createElement('canvas');
+        canvas2d.width = this.FBO[name].width;
+        canvas2d.height = this.FBO[name].height;
+        const ctx2d = canvas2d.getContext('2d');
+
+        const imageData = ctx2d.createImageData(this.FBO[name].width, this.FBO[name].height);
+        const flippedPixels = new Uint8Array(this.FBO[name].width * this.FBO[name].height * 4);
+        const rowSize = this.FBO[name].width * 4;
+        for (let y = 0; y < this.FBO[name].height; y++) {
+            const srcOffset = y * rowSize;
+            const dstOffset = (this.FBO[name].height - 1 - y) * rowSize;
+            flippedPixels.set(pixels.subarray(srcOffset, srcOffset + rowSize), dstOffset);
+        }
+        //imageData.data.set(flippedPixels);
+        imageData.data.set(pixels);
+        ctx2d.putImageData(imageData, 0, 0);
+        canvas2d.toBlob((blob) => {
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = fileName+".png";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+        }, 'image/png');
+    }
+}
+/*
 export class CoreObject{
 
     constructor(){
@@ -8,9 +185,12 @@ export class CoreObject{
         this.camera.position.z = 5;
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize( window.innerWidth, window.innerHeight );
-        this.shaders = null;
-        this.gl = this.renderer.getContext();
+        this.shaders = {};
+        const canvas = document.getElementById("canvas");
+        this.gl = canvas.getContext("webgl2");
+        //this.gl = this.renderer.getContext("webgl2");
         document.body.appendChild( this.renderer.domElement );
+        this.FBO = new FBO(this.gl,300,300);
     }
     init(){
         const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -32,33 +212,21 @@ export class CoreObject{
         const responseData = await response.json();
         return {
             name:data.name,
-            vertexCode:responseData.vertexShader,
-            fragmentCode:responseData.fragmentShader,
-            attribute:Object.values(responseData.attributes),
-            uniform:Object.values(responseData.uniforms)
+            vertexShader:responseData.vertexShader,
+            fragmentShader:responseData.fragmentShader,
+            attributes:Object.values(responseData.attributes),
+            uniforms:Object.values(responseData.uniforms)
         };
     }
     async createGPUEnvironment(){
-        let shaderData = [];
-        shaderData.push({name:'Test',file:'Test.shd'});
+        let shadersDefinition = [];
+        let shadersData = [];
+        shadersDefinition.push({name:'test',file:'Test.shd'});
+        shadersData = await Promise.all(shadersDefinition.map(data => this.loadShaderData(data)));
+        for (const shaderData of shadersData){
+            this.shaders[shaderData.name] = new Shader(this.gl,shaderData);
+        }
+    }
+}
+*/
 
-        this.shaders = await Promise.all(shaderData.map(data => this.loadShaderData(data)));
-        let x = 0;
-    }
-}
-export class FBO{
-    constructor(gl,width,height){
-        this.framebuffer = gl.createFramebuffer();
-        this.texture = gl.createTexture();
-        gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA8,width,height,0,gl.RGBA,gl.UNSIGNED_BYTE,null);
-        gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER,gl.NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
-        gl.framebufferTexture2D(gl.FRAMEBUFFER,gl.COLOR_ATTACHMENT0,gl.TEXTURE_2D,this.texture,0);
-		gl.bindTexture(gl.TEXTURE_2D,null);
-		gl.bindFramebuffer(gl.FRAMEBUFFER,null);
-    }
-}
--- static/js/GUI.js
+++ static/js/GUI.js
@@ -1,16 +1,52 @@
-import {CoreObject} from "./Core.js"
+//import {CoreObject} from "./Core.js"
+//import {FBO} from "./Core.js"
+
+import {GPUEngine} from "./Core.js"
 import {FBO} from "./Core.js"
 
-const coreObject = new CoreObject();
-await coreObject.createGPUEnvironment();
-coreObject.init();
-coreObject.renderer.setAnimationLoop( coreObject.animate );
+//const coreObject = new CoreObject();
+//await coreObject.createGPUEnvironment();
 
-const test = {};
+const width = 2048;
+const height = 2048;
 
-let N = 500000;
-for (let n=0;n<N;n++){
-    test[n] = new FBO(coreObject.gl,4096,4096);
-    if (n==N-1)
-        alert ("OK");
+const toolSet = new GPUEngine();
+await toolSet.createGPUEnvironment();
+toolSet.FBO['test'] = new FBO(toolSet.gl,width,height);
+
+
+let points = [];
+
+let position = [];
+for (let i=0;i<height;i++){
+    for (let j=0;j<width;j++){
+        position.push(-1+1/width+j*2/width,-1+1/width+i*2/width,0.0);
+    }
 }
+toolSet.shaders.test.createWebGL2BufferArrays(toolSet.gl,{1:{name:1,vertexData:{position:position},nVertices:width*height}});
+let uniforms = {color:[0.0,0.0,0.0]};
+let r = 0.0;
+let g = 0.0;
+let b = 0.0;
+let nR = 2;
+let nG = 2;
+let nB = 2;
+const t1 = Date.now();
+let count = 0;
+for (let ir=0;ir<nR;ir++){
+    r = ir*1.0/(nR-1);
+    for (let ig=0;ig<nG;ig++){
+        g = ig*1.0/(nG-1);
+        for (let ib=0;ib<nB;ib++){
+            b = ib*1.0/(nB-1);
+            toolSet.FBO[count] = new FBO(toolSet.gl,width,height);
+            toolSet.shaders.test.executeGPUCode(toolSet.gl,toolSet.FBO[count],{color:[r,g,b]});
+            toolSet.downloadTexture(count,'Test_'+count);
+            count += 1;
+        }
+    }
+}
+const t2 = Date.now();
+
+alert ((t2-t1)/1000);
+delete (toolSet.FBO);
