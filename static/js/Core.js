0045c7b8e0bba70693701ec401d383036d6155eb
jesus <jesus.robledo@lucisun.com>
2026-08-09 11:15:55 +0200

    v1.0 - Update 09/08/2026 - 11:15

--- static/js/Core.js
+++ static/js/Core.js
@@ -2,49 +2,133 @@
 import * as THREE from 'three';
 import {WebGPURenderer} from 'three/webgpu';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
-import {GPUToolSet} from 'GPUEngine';
+//import {GPUToolSet} from 'GPUEngine';
+//import {Test} from 'GPUEngine';
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+//import JSZip from 'https://esm.sh/jszip@3.10.1';
 
 // Global variables
 let scene, camera, renderer, controls;
 
 // Main canvas definition with wbGPU renderer
 scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight,0.1,1000);
-camera.position.set(0,0,120);
+scene.background = new THREE.Color(0xc0e6f5);
+camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,1,50000);
+camera.position.set(4000,4000,500);
+camera.up.set(0.0,0.0,1.0);
 renderer = new WebGPURenderer({antialias:true});
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild( renderer.domElement );
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+await renderer.init();
 controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
 
+
 // Initiate GPUEngine
-const gpuToolSet = new GPUToolSet();
-const computeNode = gpuToolSet.shaderTest().compute(gpuToolSet.size*gpuToolSet.size);
+//const gpuToolSet = new GPUToolSet();
+//const computeNode = gpuToolSet.shaderTest().compute(gpuToolSet.size*gpuToolSet.size);
+//const test = new Test();
 
 // Basic scene elements
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({map:gpuToolSet.storageTexture});
+//const geometry = new THREE.BoxGeometry(1,1,1);
+//const geometry = new THREE.PlaneGeometry (1,2);
+//const material = new THREE.MeshBasicMaterial({map:gpuToolSet.storageTexture, side:THREE.DoubleSide});
+//const material = new THREE.MeshBasicMaterial({color:0x000000, side:THREE.DoubleSide});
 
-const nBoxes = 10000;
-const instancedMesh = new THREE.InstancedMesh(geometry, material, nBoxes);
+// Include Lighting
+const directionalLight = new THREE.DirectionalLight (0xffffff,3.0);
+//directionalLight.up = new THREE.Vector3(0.0,0.0,1.0);
+directionalLight.position.set (5000.0,-5000.0,1000.0);
+directionalLight.castShadow = true;
+//directionalLight.shadow.camera.position.set(0.0,0.0,2500);
+directionalLight.shadow.camera.near = 500;
+directionalLight.shadow.camera.far = 10000;
+directionalLight.shadow.camera.left = -5000;
+directionalLight.shadow.camera.right = 5000;
+directionalLight.shadow.camera.top = 5000;
+directionalLight.shadow.camera.bottom = -5000;
+directionalLight.shadow.mapSize.set(4096,4096);
+//directionalLight.shadow.camera.updateProjectionMatrix();
+scene.add(directionalLight);
+
+// Load the terrain
+const loader = new OBJLoader();
+const terrain = await loader.loadAsync('static/Terrain_5km.obj');
+terrain.material = new THREE.MeshStandardMaterial();
+terrain.children[0].castShadow = true;
+terrain.children[0].receiveShadow = true;
+terrain.position.set(0.0,0.0,0);
+scene.add(terrain);
+
+// Add some helpers
+scene.add(new THREE.AxesHelper(500));
+const shadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+const shadowHelper2 = new THREE.DirectionalLightHelper(directionalLight,500);
+scene.add(shadowHelper);
+scene.add(shadowHelper2);
+
+// Load a bunch of modules
+const nX = 250;
+const nY = 1250;
+const dX = 3.0;
+const dY = 1.5;
+//const pvModule = new THREE.PlaneGeometry (1,2);
+const pvModule = new THREE.BoxGeometry(1,2,0.1);
+const textureLoader = new THREE.TextureLoader();
+const baseColorTexture = textureLoader.load('static/Poly_6x12_Front.jpg');
+baseColorTexture.colorSpace = THREE.SRGBColorSpace;
+const material = new THREE.MeshStandardMaterial({map:baseColorTexture, side:THREE.DoubleSide});
+const instancedMesh = new THREE.InstancedMesh(pvModule, material, nX*nY);
 const dummy = new THREE.Object3D();
-for (let i = 0; i < nBoxes; i++) {
-  dummy.position.set((Math.random() - 0.5) * 100,
-                     (Math.random() - 0.5) * 100,
-                     (Math.random() - 0.5) * 100);
-  dummy.updateMatrix();
-  instancedMesh.setMatrixAt(i, dummy.matrix);
+for (let j=0; j<nY; j++){
+    for (let i=0; i<nX; i++) {
+        dummy.position.set(-0.5*(nX-1)*dX + dX*i,
+                           -0.5*(nY-1)*dY + dY*j,
+                            400);
+        dummy.rotation.order = 'ZYX';
+        dummy.rotation.set(45.0*Math.PI/180,
+                           0.0,
+                           Math.PI/2);
+        dummy.updateMatrix();
+        instancedMesh.setMatrixAt(j*nX+i, dummy.matrix);
+    }
 }
-
+instancedMesh.castShadow = true;
+instancedMesh.receiveShadow = true;
+instancedMesh.computeBoundingSphere();
 scene.add(instancedMesh);
 
+/*
+const computeNode = test.generateIndicesFn().compute(test.count);
+await renderer.computeAsync(computeNode);
+const arrayBuffer = await renderer.getArrayBufferAsync(test.storageAttr);
+const resultFloats = new Float32Array(arrayBuffer);
+const zip = new JSZip();
+zip.file("floats_256.bin", resultFloats.buffer);
+const zipBlob = await zip.generateAsync({ type: "blob",compression: "DEFLATE", compressionOptions: { level: 9 } });
+const loadedZip = await JSZip.loadAsync(zipBlob);
+const buffer = await loadedZip.file("floats_256.bin").async("arraybuffer");
+const extractedFloats = new Float32Array(buffer);
+
+
+const url = URL.createObjectURL(zipBlob);
+const link = document.createElement('a');
+link.href = url;
+link.download = "data.zip";
+document.body.appendChild(link);
+link.click();
+document.body.removeChild(link);
+URL.revokeObjectURL(url);
+*/
+
 // Animation loop
-await renderer.init();
 function animate() {
     controls.update();
-    renderer.compute(computeNode);
+    //renderer.compute(computeNode);
     renderer.render(scene,camera);
 }
 renderer.setAnimationLoop(animate);
--- static/js/GPUEngine.js
+++ static/js/GPUEngine.js
@@ -1,6 +1,6 @@
 // Import the required classes
 import * as THREE from 'three';
-import {Fn, textureStore, instanceIndex, uvec2, vec4, float} from 'three/tsl';
+import {Fn, textureStore, storage, instanceIndex, uvec2, vec4, float, sin, time} from 'three/tsl';
 
 export class GPUToolSet{
 
@@ -13,10 +13,23 @@ export class GPUToolSet{
             const posX = instanceIndex.mod(this.size);
             const posY = instanceIndex.div(this.size);
             const coord2D = uvec2(posX,posY);
-            const r = float(posX).div(this.size-1);
+            const r = float(posX).div(this.size-1).mul(sin(time));
             const g = float(posY).div(this.size-1);
             const b = 0.0;
             textureStore(this.storageTexture, coord2D, vec4(r,g,b,1.0)).toWriteOnly();
+        });
+    }
+}
+
+export class Test{
+    constructor(){
+        this.count = 2048*2048;
+        const floatArray = new Float32Array(this.count);
+        this.storageAttr = new THREE.StorageBufferAttribute(floatArray, 1);
+        this.storageBufferNode = storage(this.storageAttr, 'float', this.count);
+        this.generateIndicesFn = Fn(() => {
+            const idx = instanceIndex;
+            this.storageBufferNode.element(idx).assign(3.14);
         });
     }
 }
